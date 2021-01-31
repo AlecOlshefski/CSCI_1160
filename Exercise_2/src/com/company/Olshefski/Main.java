c60e3152f73ccf946272a5503ebcfeb31784343e
alecolshefski <aolshefski@student.cscc.edu>
2021-01-30 22:21:26 -0500

    ex2

--- Exercise_2/src/com/company/Olshefski/Main.java
+++ Exercise_2/src/com/company/Olshefski/Main.java
@@ -2,9 +2,16 @@ package com.company.Olshefski;
 
 public class Main {
 
-    public static void main(String[] args) {
-	String week[]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-	double temp[]={45.0,29.0,10.0,22.0,41.0,28.0,33.0};
-	double prob[]={95,60,25,5,0,75,90};
-    }
+	public static void main(String[] args) {
+		String week[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+		double temp[] = {45.0, 29.0, 10.0, 22.0, 41.0, 28.0, 33.0};
+		double prob[] = {95, 60, 25, 5, 0, 75, 90};
+
+		for (int i = 0; i <= 6; i++) {
+			if (temp[i] <= 32 && prob[i] > 50) {
+				System.out.println("its likely to snow on: " + week[i]);
+			}
+
+		}
+	}
 }
