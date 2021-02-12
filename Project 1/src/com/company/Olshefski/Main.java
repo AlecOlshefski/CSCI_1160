ea4ce5f2cb682b5c19f66e394fbf3c6378aedc78
alecolshefski <aolshefski@student.cscc.edu>
2021-02-12 12:22:44 -0500

    2.12.21

--- Project 1/src/com/company/Olshefski/Main.java
+++ Project 1/src/com/company/Olshefski/Main.java
@@ -1,9 +1,16 @@
 package com.company.Olshefski;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
+
+        //Array List created named task list
+        ArrayList<String> taskList = new ArrayList<>();
+
         Scanner input = new Scanner(System.in);
         System.out.println("Please choose an option: \n" +
                 "(1) Add a task. \n" +
@@ -17,44 +24,31 @@ public class Main {
 
             switch (menuInput) {
                     case "1":
+                    addTask(taskList);
                     System.out.println("Task successfully added!");
-                    System.out.println("Please choose an option: \n" +
-                            "(1) Add a task. \n" +
-                            "(2) Remove a task. \n" +
-                            "(3) Update a task. \n" +
-                            "(4) List all tasks. \n" +
-                            "(0) Exit. \n");
+                    displayMenu();
                     break;
+
                 case "2":
+                    removeTask(taskList);
                     System.out.println("Task successfully removed!");
-                    System.out.println("Please choose an option: \n" +
-                            "(1) Add a task. \n" +
-                            "(2) Remove a task. \n" +
-                            "(3) Update a task. \n" +
-                            "(4) List all tasks. \n" +
-                            "(0) Exit. \n");
+                    displayMenu();
                     break;
+
                 case "3":
+                    updateTask(taskList);
                     System.out.println("Task successfully updated!");
-                    System.out.println("Please choose an option: \n" +
-                            "(1) Add a task. \n" +
-                            "(2) Remove a task. \n" +
-                            "(3) Update a task. \n" +
-                            "(4) List all tasks. \n" +
-                            "(0) Exit. \n");
+                    displayMenu();
                     break;
+                    
                 case "4":
                     System.out.println("Your Tasks recorded are...");
-
-                    System.out.println("Please choose an option: \n" +
-                            "(1) Add a task. \n" +
-                            "(2) Remove a task. \n" +
-                            "(3) Update a task. \n" +
-                            "(4) List all tasks. \n" +
-                            "(0) Exit. \n");
+                    displayAllTasks(taskList);
+                    displayMenu();
                     break;
+
                 case "0":
-                    System.exit(0);
+                    exitProgram();
                 default:
                     System.out.println("Please insert one of the number from the menu (0-4)");
 
@@ -63,6 +57,57 @@ public class Main {
 
 
     }
+
 }
+    //Displays menu after input
+    private static void displayMenu(){
+        System.out.println("Please choose an option: \n" +
+                "(1) Add a task. \n" +
+                "(2) Remove a task. \n" +
+                "(3) Update a task. \n" +
+                "(4) List all tasks. \n" +
+                "(0) Exit. \n");
+    }
+
+    //For input 1 (Adds a task)
+    static ArrayList<String> addTask(ArrayList <String> taskList){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a description of task");
+        String userInput = input.nextLine();
+        taskList.add(userInput);
+        return taskList;
+    }
+    //For input 2 (removes a task)
+    static ArrayList<String> removeTask(ArrayList<String>taskList){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the index of the task you want to remove");
+        int userInput = input.nextInt();
+        taskList.remove(userInput);
+        return taskList;
+
+    }
+
+    //For input 3 (Updates a task)
+    static ArrayList<String> updateTask(ArrayList<String> taskList){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter an index value of task to update");
+        int userInput = input.nextInt();
+        System.out.println("What would you like to change the task to?");
+        String desc = input.next();
+        taskList.set(userInput,desc);
+        return taskList;
+
+    }
+
+    //for input 4 (Displays all tasks)
+    static void displayAllTasks(ArrayList<String> taskList){
+        System.out.println(taskList);
+    }
+
+    //for input 0 (Closes program)
+    static void exitProgram(){
+        System.exit(0);
+    }
+
 }
 
