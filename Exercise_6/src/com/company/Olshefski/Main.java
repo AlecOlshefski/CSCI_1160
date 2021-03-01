20839211245db56a5dd70feb4de098862102561c
AlecOlshefski <77692640+AlecOlshefski@users.noreply.github.com>
2021-02-28 23:51:38 -0500

    Update Main.java

--- Exercise_6/src/com/company/Olshefski/Main.java
+++ Exercise_6/src/com/company/Olshefski/Main.java
@@ -2,8 +2,6 @@ package com.company.Olshefski;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
